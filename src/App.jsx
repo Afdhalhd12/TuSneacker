50aef99d248e23dff4212a382a2ee5fea0f98cbf
Whitefox23321 <afdhalhadisolahudin@gmail.com>
2026-05-08 11:06:16 +0700

    fix image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import coffee from "./assets/product.png";
 import shadow from "./assets/shadow.png";
 import beans from "./assets/beans.png";
-import black from "./assets/blackwhite.jpg";
+import black from "./assets/beast.jpg";
 import latte from "./assets/latte.jpg";
 import latte1 from "./assets/latte1.jpg";
 import latte2 from "./assets/latte2.jpg";
+import cafee from "./assets/cafee.jpg";
+import italian from "./assets/italian.jpg";
+import croisant from "./assets/bar.jpg";
+import hot from "./assets/tuang.jpg";
 import ButtonComp from "./components/ButtonComp";
 import { MdOutlineHexagon } from "react-icons/md";
 import { CiCoffeeCup } from "react-icons/ci";
@@ -86,8 +90,8 @@ export default function App() {
       duration: 3,
       ease: "sine.inOut",
       repeat: -1,
-      yoyo : true
-    
+      yoyo: true
+
     })
 
     gsap.set(".explore", {
@@ -155,38 +159,109 @@ export default function App() {
 
           </div>
         </div>
-
+        {/* Coffee */}
         <div className="mt-10">
           <div className="flex justify-between">
-            <h1 className="explore text-4xl font-bold text-[#E8A736]">EXPLORE OUR <span className="text-[#6e501d]">COLLECTION</span></h1>
-            <h1 className="text-1xl font-bold text-[#E8A736]">View <span className="text-[#6e501d]">All</span></h1>
+            <h1 className="explore text-4xl font-bold text-[#E8A736] font-serif">EXPLORE OUR <span className="text-[#6e501d]">COLLECTION</span></h1>
+            <h1 className="text-1xl font-bold text-[#E8A736] font-serif">View <span className="text-[#6e501d]">All</span></h1>
           </div>
           <div className="grid grid-cols-8 gap-4 mt-10">
             <div className="col-span-4 relative">
               <img src={black} className="rounded-2xl  h-105" />
-              <p className="absolute inset-1 flex items-end text-white p-2 pb-10 text-1xl">Coffee Latte</p>
-              <h3 className="absolute inset-0 flex items-end text-white font-bold p-3 text-3xl">Coffee Latte</h3>
+              <p className="absolute inset-1 flex items-end text-white p-2 pb-10 text-1xl font-serif">Coffee Latte</p>
+              <h3 className="absolute inset-0 flex items-end text-white font-bold p-3 text-3xl font-serif">Coffee Latte</h3>
             </div>
             <div className="col-span-2 relative">
-              <img src={latte} className="rounded-2xl  h-105" />
-              <p className="absolute inset-1 flex items-end text-white p-2 pb-10 text-1xl">Coffee Latte</p>
-              <h3 className="absolute inset-0 flex items-end text-white font-bold p-3 text-3xl">Coffee Latte</h3>
+              <img src={italian} className="rounded-2xl  h-105 w-80" />
+              <p className="absolute inset-1 flex items-end text-white p-2 pb-10 text-1xl font-serif">Coffee Latte</p>
+              <h3 className="absolute inset-0 flex items-end text-white font-bold p-3 text-3xl font-serif">Coffee Latte</h3>
             </div>
             <div className="col-span-2">
               <div className="relative">
-                <img src={latte1} className="rounded-2xl  h-50 w-75" />
-                <p className="absolute inset-1 flex items-end text-white p-2 pb-10 text-1xl">Coffee Latte</p>
-                <h3 className="absolute inset-0 flex items-end text-white font-bold p-3 text-3xl">Coffee Latte</h3>
+                <img src={hot} className="rounded-2xl  h-50 w-75" />
+                <p className="absolute inset-1 flex items-end text-white p-2 pb-10 text-1xl font-serif">Coffee Latte</p>
+                <h3 className="absolute inset-0 flex items-end text-white font-bold p-3 text-3xl font-serif">Coffee Latte</h3>
               </div>
               <div className="relative">
-                <img src={latte2} className="rounded-2xl  h-50 w-75 mt-5" />
-                <p className="absolute inset-1 flex items-end text-white p-2 pb-10 text-1xl">Coffee Latte</p>
-                <h3 className="absolute inset-0 flex items-end text-white font-bold p-3 text-3xl">Coffee Latte</h3>
+                <img src={croisant} className="rounded-2xl  h-50 w-75 mt-5" />
+                <p className="absolute inset-1 flex items-end text-white p-2 pb-10 text-1xl font-serif">Coffee Latte</p>
+                <h3 className="absolute inset-0 flex items-end text-white font-bold p-3 text-3xl font-serif">Coffee Latte</h3>
               </div>
             </div>
-
           </div>
         </div>
+
+        <div className="mt-10">
+          <h1 className="explore text-4xl font-bold text-[#E8A736] font-serif">EXPLORE OUR <span className="text-[#6e501d]">COLLECTION</span></h1>
+          <div className="mt-5">
+            <div className="grid grid-cols-8 gap-5">
+              <div className="col-span-4">
+
+                <div className="mt-5">
+                  <div className="shadow-2xl bg-[#eeeeec] rounded-2xl">
+                    <div className="grid grid-cols-8">
+                      <div className="col-span-2">
+                        <div className="p-3">
+                          <img src={latte} className="rounded-2xl h-25 w-25" />
+                        </div>
+                      </div>
+                      <div className="col-span-6">
+                        <div className="p-3">
+                          <h1 className="font-bold text-3xl text-black font-serif">Latte</h1>
+                          <p className="text-gray-700 font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, nam magni doloremque sequi doloribus placeat eveniet, asperiores odit minus deleniti</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="mt-5">
+                  <div className="shadow-2xl bg-[#E8A736] rounded-2xl">
+                    <div className="grid grid-cols-8">
+                      <div className="col-span-2">
+                        <div className="p-3">
+                          <img src={latte} className="rounded-2xl h-25 w-25" />
+                        </div>
+                      </div>
+                      <div className="col-span-6">
+                        <div className="p-3">
+                          <h1 className="font-bold text-3xl text-white font-serif">Latte</h1>
+                          <p className="text-white font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, nam magni doloremque sequi doloribus placeat eveniet, asperiores odit minus deleniti</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="mt-5">
+                  <div className="shadow-2xl bg-black rounded-2xl">
+                    <div className="grid grid-cols-8">
+                      <div className="col-span-2">
+                        <div className="p-3">
+                          <img src={latte} className="rounded-2xl h-25 w-25" />
+                        </div>
+                      </div>
+                      <div className="col-span-6">
+                        <div className="p-3">
+                          <h1 className="font-bold text-3xl text-white font-serif">Latte</h1>
+                          <p className="text-white font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, nam magni doloremque sequi doloribus placeat eveniet, asperiores odit minus deleniti</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+
+              <div className="col-span-4">
+              <div className="mt-5">
+                <img src={black} className="rounded-2xl w-160 h-109"/>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
 
     </>
